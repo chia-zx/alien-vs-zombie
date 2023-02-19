79f3d373ecd4a6625390dd45654a20b91dd522cf
chia-zx <123618041+chia-zx@users.noreply.github.com>
2023-02-19 17:25:42 +0800

    Modified

--- direction.cpp
+++ direction.cpp
@@ -1,19 +1,87 @@
 #include "h/other.h"
 #include <iostream>
-#include <vector>
 #include <iomanip>
 using namespace std;
 
-void Game::changeDirection(vector < vector<char> >& boards)
+bool validArrow(char changeArrow)
 {
-    int changeRow;
-    int changeColumn;
-	char currentArrow;
-    char changeArrow;
+    bool valid = false;
+    char direction[] = {'^', 'v' , '<' , '>'};
 
-    cout << "Enter row -> "; cin >> changeRow;
-    cout << "Enter column -> "; cin >> changeColumn;
+    for (int i = 0; i < sizeof(direction); ++i)
+    {
+        if (direction[i] == changeArrow)
+        {
+            valid = true;
+            break;
+        }
+    }
+    return valid;
+}
+
+bool Game::isArrow(int changeRow, int changeColumn)
+{
+    bool arrow = false;
+    char object = boards[changeRow - 1][changeColumn - 1];
+    if (object == '^' || object == 'v' || object == '<' || object == '>')
+    {
+        arrow = true;
+    }
+    else
+    {
+        cout << "The row and column number entered is not matched to an arrow !" << endl;
+    }
+    return arrow;
+}
+
+bool Game::validColumn(int changeColumn)
+{
+    int x = changeColumn - 1;
+	return ((x > -1) && (x < columns));
+}
+
+bool Game::validRow(int changeRow)
+{
+    int y = changeRow - 1;
+	return ((y > -1) && (y < rows));
+}
+
+void Game::changeDirection()
+{
+    int changeRow, changeColumn;
+	char currentArrow, changeArrow;
+
+    while (true)
+    {
+        cout << "Enter row -> "; cin >> changeRow;
+        while (!validRow(changeRow))
+        {
+            cout << "Invalid input! Please enter a valid row number!" << endl;
+            cout << "Enter row -> "; cin >> changeRow;
+        }
+        cout << endl;
+
+        cout << "Enter column -> "; cin >> changeColumn;
+        while (!validColumn(changeColumn))
+        {
+            cout << "Invalid input! Please enter a valid column number!" << endl;
+            cout << "Enter column -> "; cin >> changeColumn;
+        }
+        cout << endl;
+
+        if (isArrow(changeRow, changeColumn))   // check whether the value is match to arrow
+        {
+            break;
+        }
+    } 
+
     cout << "Enter direction -> "; cin >> changeArrow;
+	while (!validArrow(changeArrow))
+    {
+        cout << "Invalid input! Please enter a valid arrow !" << endl;
+        cout << "Enter direction -> "; cin >> changeArrow;
+    }
+    cout << endl;
     
     int y = changeRow - 1;
 	int x = changeColumn - 1;
@@ -22,5 +90,5 @@ void Game::changeDirection(vector < vector<char> >& boards)
 
     cout << "Arrow " << currentArrow << " is changed to " << changeArrow << endl;
 
-    Pause();    // press any to continue
+    newScreen();    // press any to continue
 }
